63c07045be2521cf3e6ae46413cf5be42abc7572
rashmikt <79697993+rashmikt@users.noreply.github.com>
2023-10-22 16:29:49 +0200

    Added Navigation bar and logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+
+const Shop = () => {
+  return <h1>The Shop page</h1>;
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' index element={<Home />} />
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
